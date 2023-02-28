b3207936202de2e7b6e6a2334d95b34aa1714eb5
Zoya Anand <zoya.anand0509@gmail.com>
2023-02-28 12:17:19 +0530

    CC-FEB-PYTHON-DEVELOPEMENT

--- Task3-Python Flappy Bird Game/tempCodeRunnerFile.py
+++ Task3-Python Flappy Bird Game/tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
-elif 'play series' in query:
-            series_path="C:\\Users\\KIIT\\Videos\\ITS OKAY TO NOT BE OKAY"
-            eps=os.listdir(series_path)
-            os.startfile(os.path.join(series_path,eps[0]))
+message
